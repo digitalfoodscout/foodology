3a0b9f00fb1643753046d22459a8f03d1589cc1d
Daniel Sendzik <d.sendzik@gmail.com>
2017-05-18 19:26:11 +0200

    Changed group and name

--- src/main/java/Foodology.java
+++ src/main/java/Foodology.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Owlapitest {
+public class Foodology {
     public static void main(String args[]) {
         OWLScout owlScout;
 
