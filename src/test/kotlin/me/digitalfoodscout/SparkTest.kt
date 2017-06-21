bd6a7bf2f9e0074a15c300b2266d48df898c44c5
Daniel Sendzik <d.sendzik@gmail.com>
2017-06-21 13:22:18 +0200

    (feat) added basic test with Unirest and AssertJ

--- src/test/kotlin/me/digitalfoodscout/SparkTest.kt
+++ src/test/kotlin/me/digitalfoodscout/SparkTest.kt
@@ -1,7 +1,11 @@
 package me.digitalfoodscout
 
+import com.mashape.unirest.http.Unirest
 import org.junit.ClassRule
 import org.junit.Test
+
+import org.assertj.core.api.Assertions.*
+import org.json.JSONArray
 
 class SparkTest {
     companion object {
@@ -11,6 +15,20 @@ class SparkTest {
 
     @Test
     fun testBasicRule() {
-        println("It works!")
+        val jsonResponse = Unirest.get("http://localhost:4567/occursWith/Erbrechen")
+                .asJson()
+
+        assertThat(jsonResponse.status).isEqualTo(200)
+
+        val jsonNode = jsonResponse.body
+        assertThat(jsonNode.isArray).isFalse()
+
+        val jsonObject = jsonNode.`object`
+
+        val listOften = jsonObject.get("often") as JSONArray
+        val listSeldom = jsonObject.get("seldom") as JSONArray
+
+        assertThat(listOften).hasSize(1).contains("Lactose")
+        assertThat(listSeldom).hasSize(0)
     }
 }
